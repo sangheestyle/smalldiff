c02bc876169626c5b5da86bc712f4ab8160197fc
Sanghee Kim <sangheestyle@gmail.com>
2015-08-05 16:03:01 -0600

    FIXED: runtime error: nil pointer dereference due to pushed_at with nil
    a pushed_at value with nil arises runtime error, nil pointer derefernce.
    It can be happend sometimes. So, I use defer-recover to recover
    control.
    
    http://blog.golang.org/defer-panic-and-recover

--- server.go
+++ server.go
@@ -102,28 +102,36 @@ func CrawlGithubRepos(query string) (created int, updated int, err error) {
 		}
 
 		for _, r := range repos.Repositories {
-			repo := &Repo{
-				ID:              r.ID,
-				FullName:        r.FullName,
-				GCreatedAt:      r.CreatedAt.Time,
-				GPushedAt:       r.PushedAt.Time,
-				GUpdatedAt:      r.UpdatedAt.Time,
-				GitURL:          r.GitURL,
-				Langauge:        r.Language,
-				ForksCount:      r.ForksCount,
-				OpenIssuesCount: r.OpenIssuesCount,
-				WatchersCount:   r.WatchersCount,
-				Size:            r.Size,
-			}
+			// CreatedAt with nil makes panic
+			func() {
+				defer func() {
+					if r := recover(); r != nil {
+						fmt.Println("Recovered in repos", r)
+					}
+				}()
+				repo := &Repo{
+					ID:              r.ID,
+					FullName:        r.FullName,
+					GCreatedAt:      r.CreatedAt.Time,
+					GPushedAt:       r.PushedAt.Time,
+					GUpdatedAt:      r.UpdatedAt.Time,
+					GitURL:          r.GitURL,
+					Langauge:        r.Language,
+					ForksCount:      r.ForksCount,
+					OpenIssuesCount: r.OpenIssuesCount,
+					WatchersCount:   r.WatchersCount,
+					Size:            r.Size,
+				}
 
-			// Create new and update existed one
-			if Db.Where("ID = ?", repo.ID).First(repo).RecordNotFound() {
-				created += 1
-				Db.Create(repo)
-			} else {
-				updated += 1
-				Db.Save(repo)
-			}
+				// Create new and update existed one
+				if Db.Where("ID = ?", repo.ID).First(repo).RecordNotFound() {
+					created += 1
+					Db.Create(repo)
+				} else {
+					updated += 1
+					Db.Save(repo)
+				}
+			}()
 		}
 
 		if resp.NextPage == 0 {
